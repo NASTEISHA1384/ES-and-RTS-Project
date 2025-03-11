79a4617fa65e3020a404e62085c19e1f11c852bb
NASTEISHA1384 <elninobrawl1384@gmail.com>
2025-03-11 12:44:10 +0330

    Update .gitignore file. Test systemc via a welcom.cpp file

---
+++ welcome.cpp
@@ -0,0 +1,15 @@
+#include "systemc.h"
+
+int sc_main(int argc, char* argv[]) {
+    std::cout << "Hello, SystemC!" << std::endl;
+    return 0;
+}
+
+// The results are inserted below:
+
+/*
+SystemC 3.0.1-Accellera --- Mar 11 2025 11:39:02
+Copyright (c) 1996-2024 by all Contributors,    
+ALL RIGHTS RESERVED
+Hello, SystemC!
+*/
